4d435c1f84644df17efc77a08abd8130a5b9cda6
George Croudace <georgecmtb@gmail.com>
2022-07-31 13:18:17 +0100

    moved Board class to pieces.py so it can access the piece classes without them needing to be imported in the main program

--- pieces.py
+++ pieces.py
@@ -3,6 +3,41 @@ Holds all classes for chess pieces
 """
 
 
+class Board():
+    """Aggregate class to store pieces and board operations
+
+    Attributes:
+        pieces (List[object]): List of the pieces on the board
+
+    Methods:
+        move: Take user input and call the required piece methods
+        update: Update the game board with the result of the piece methods
+        display: Display the game board in the terminal
+        setup: Creates game board from fen notation string
+
+    """
+
+    def __init__(self, white_turn=True, moves_made=0, white_score=0, black_score=0) -> None:
+        self.white_turn = white_turn
+        self.moves_made = moves_made
+        self.white_score = white_score
+        self.black_score = black_score
+        self.board = [None] * 64
+        self.captured = []
+
+    def move(self):
+        """Takes input from user and calls the corresponding methods in piece classes"""
+
+    def update(self):
+        """Updates the game board with the result of the piece methods"""
+
+    def display(self):
+        """Displays the game board in the terminal"""
+
+    def setup(self, fen: str) -> None:
+        """Creates game board contaning piece objects from a fen notation string template"""
+
+
 class Piece:
     """Base piece type
 
